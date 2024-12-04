a59a6f2724b80028e5cf71bd72bcd84c8921e245
QuangAT <forsubcriptionbeta@gmail.com>
2024-12-04 03:53:10 -0500

    Task 8 commit

--- server.py
+++ server.py
@@ -1,11 +1,13 @@
-from EmotionDetection.emotion_detection import emotion_detector
+"""Module providing a function printing python version."""
 from flask import Flask,render_template,request
+from EmotionDetection.emotion_detection import emotion_detector
 
-#Initiate the flask app : 
+#Initiate the flask app:
 app = Flask("Emotion Detection")
 
 @app.route("/emotionDetector")
 def sent_emotion_detector():
+    '''return request result'''
     text_to_analyse = request.args.get('textToAnalyze')
     emotions = emotion_detector(text_to_analyse)
     anger_score = emotions['anger']
@@ -16,7 +18,11 @@ def sent_emotion_detector():
     dominant_emotion = emotions['dominant_emotion']
     if dominant_emotion is None:
         return "Invalid text!Please try again!"
-    return f"For the given statement, the system response is 'anger\': {anger_score}, 'disgust\': {disgust_score}, 'fear\': {fear_score}, 'joy\': {joy_score}, and 'sadness\': {sadness_score}. The dominant education is {dominant_emotion}"
+    return(
+        f"For the given statement, the system response is 'anger\': {anger_score},"
+        f"'disgust\': {disgust_score}, 'fear\': {fear_score}, 'joy\': {joy_score}," 
+        f" and 'sadness\': {sadness_score}. The dominant education is {dominant_emotion}"
+    )
 
 @app.route("/")
 def render_index_page():
@@ -26,6 +32,6 @@ def render_index_page():
     return render_template("index.html")
 
 if __name__ == "__main__":
-    ''' This functions executes the flask app and deploys it on localhost:5000
-    '''
+    #This functions executes the flask app and deploys it on localhost:5000
     app.run(host="0.0.0.0",port=5000)
+    
